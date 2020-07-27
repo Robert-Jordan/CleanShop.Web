076bd498bfa01f1411177d6c7e5528ea669138f1
Alexander Izmiestiev <a.izmiestiev@gmail.com>
2020-07-27 17:56:33 +0300

    Implemented working shopping cart, added displaying of icons in cms

---
+++ CleanShop.Core/Extensions/PublishedContentExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web;
+using Vendr.Core;
+using Vendr.Core.Api;
+using Vendr.Core.Models;
+
+namespace CleanShop.Core.Extensions
+{
+    public static class PublishedContentExtensions
+    {
+        public static string GetProductReference(this IPublishedContent content)
+        {
+            return content.Key.ToString();
+        }
+
+        public static IProductSnapshot AsVendrProduct(this IPublishedContent content) 
+        {
+            return VendrApi.Instance.GetProduct(content.GetProductReference());
+        }
+
+        public static Price CalculatePrice(this IPublishedContent conent)
+        {
+            return conent.AsVendrProduct()?.CalculatePrice();
+        }
+
+        public static StoreReadOnly GetStore(this IPublishedContent conent)
+        {
+            return conent.Root().Value<StoreReadOnly>("store");
+        }
+
+        public static OrderReadOnly GetCurrentOrder(this IPublishedContent conent)
+        {
+            return VendrApi.Instance.GetCurrentOrder(conent.GetStore().Id);
+        }
+    }
+}
